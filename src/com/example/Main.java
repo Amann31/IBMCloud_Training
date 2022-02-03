3755690e617978596a402af661e48294193c4007
Aman <aman.sharma0463>
2022-02-03 10:39:10 +0530

    Implemented boxing/unboxing

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -1,22 +1,39 @@
 package com.example;
 
+import java.util.Scanner;
+import java.util.UUID;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
 	// write your code here
         //primitive datatype
-        int a =100;
+       // int a =100;
         //boxing
-        Integer x=new Integer(100);
+       // Integer x=new Integer(100);
 
         //auto-boxing
-        Integer y=100;
+        //Integer y=100;
 
         //unboxing
        // int b=x.intValue();
 
         //auto-unboxing
-        int result=x;
+        //int result=x;
 
+        Beer myBeer= new Beer();
+        String[] str = UUID.randomUUID().toString().split("-");
+        myBeer.setBeerId(str[3]);
+        System.out.println("Enter beer name: ");
+        String name=s.next();
+        myBeer.setBeerName(name);
+        System.out.println("Enter cost per unit: ");
+        double cost =s.nextDouble();
+        myBeer.setCostPerUnit(cost);
+        System.out.println("Enter Stock: ");
+        int stock = s.nextInt();
+        myBeer.setUnitInStock(stock);
+        System.out.println(myBeer);
     }
 }
