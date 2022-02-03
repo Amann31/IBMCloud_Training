1eeeb366d1c85325d3617a2eb91752a18a22d957
Aman <aman.sharma0463>
2022-02-03 10:01:00 +0530

    Implemented boxing/unboxing

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -4,5 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        //primitive datatype
+        int a =100;
+        //boxing
+        Integer x=new Integer(100);
+
+        //auto-boxing
+        Integer y=100;
+
+        //unboxing
+       // int b=x.intValue();
+
+        //auto-unboxing
+        int result=x;
+
     }
 }
