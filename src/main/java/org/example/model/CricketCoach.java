b70e2c2298487fc8923de8f8c413b594b8a955fe
Amann31 <42437395+Amann31@users.noreply.github.com>
2022-02-08 17:35:33 +0530

    initial ioc implementation

---
+++ src/main/java/org/example/model/CricketCoach.java
@@ -0,0 +1,9 @@
+package org.example.model;
+
+public class CricketCoach implements Coach{
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice Spin Bowling Today";
+    }
+}
